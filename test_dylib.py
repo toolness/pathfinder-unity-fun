5eeb6fa60cbd2ebc9b70e5bffe9b22ad16c5c7c9
Atul Varma <varmaa@gmail.com>
2019-05-11 20:16:36 -0400

    Improve test_dylib a bit.

--- test_dylib.py
+++ test_dylib.py
@@ -1,18 +1,25 @@
+import sys
 from pathlib import Path
 from ctypes import *
 
 MY_DIR = Path(__file__).parent.resolve()
 
-BUILD_TYPE = "debug"
+BUILD_TYPE = "release" if "--release" in sys.argv else "debug"
 
 DLL_PATH = MY_DIR / "target" / BUILD_TYPE / "pathfinder_c_api_fun.dll"
 
 DLL_STR = str(DLL_PATH)
 
+print(f"Size of DLL is {DLL_PATH.stat().st_size} bytes.")
+
 cdll.LoadLibrary(DLL_STR)
 
 fun = CDLL(DLL_STR)
 
-assert fun.boop(2) == 642
+result = fun.boop(2)
+
+print(f"result is {result}.")
+
+assert result == 642
 
 print("Hooray, it works!")
