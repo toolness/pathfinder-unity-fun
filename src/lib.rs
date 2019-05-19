e0182b210fa127972cfa85ef14795cf61f656df4
Atul Varma <varmaa@gmail.com>
2019-05-19 19:00:02 -0400

    Add a plugin state.

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,37 @@
-static mut LOADS: i32 = 0;
+struct PluginState;
+
+static mut PLUGIN_STATE: Option<PluginState> = None;
 
 // This is called by Unity when the plugin is loaded.
 #[no_mangle]
 pub extern "stdcall" fn UnityPluginLoad(_ptr: *mut ::libc::c_void) {
-    unsafe { LOADS += 1; }
+    unsafe {
+        assert!(PLUGIN_STATE.is_none());
+        PLUGIN_STATE = Some(PluginState {});
+    }
 }
 
 // This is called by Unity when the plugin is about to be unloaded.
 #[no_mangle]
 pub extern "stdcall" fn UnityPluginUnload() {
+    unsafe {
+        assert!(PLUGIN_STATE.is_some());
+        PLUGIN_STATE = None;
+    }
+}
+
+fn get_plugin_state_mut() -> &'static mut Option<PluginState> {
+    unsafe {
+        return &mut PLUGIN_STATE;
+    }
 }
 
 #[no_mangle]
 pub extern "stdcall" fn boop_stdcall(x: i32) -> i32 {
-    let loads = unsafe { LOADS };
-    loads + x
+    match get_plugin_state_mut() {
+        None => -1,
+        Some(_) => 50 + x
+    }
 }
 
 #[cfg(test)]
@@ -22,6 +39,6 @@ mod tests {
     #[test]
     fn it_works() {
         use super::boop_stdcall;
-        assert_eq!(boop_stdcall(2), 2);
+        assert_eq!(boop_stdcall(2), -1);
     }
 }
