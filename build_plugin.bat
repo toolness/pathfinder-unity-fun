cd163ad3851760fd3bcbfa4483df384e02803d69
Atul Varma <varmaa@gmail.com>
2019-05-19 18:31:55 -0400

    Copy DLL into dist dir.

--- build_plugin.bat
+++ build_plugin.bat
@@ -1,1 +1,1 @@
-cargo build && copy target\debug\pathfinder_c_api_fun.dll unity-project\Assets
+cargo build && copy target\debug\pathfinder_c_api_fun.dll unity-project\Assets && copy target\debug\pathfinder_c_api_fun.dll dist\unity-project_Data\Plugins
