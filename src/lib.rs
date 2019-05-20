5840de50ab29601b0582b7219c4fcbbab77561ff
Atul Varma <varmaa@gmail.com>
2019-05-20 06:44:44 -0400

    Log renderer type and gfx device events.

--- src/lib.rs
+++ src/lib.rs
@@ -5,21 +5,37 @@ use std::io::prelude::*;
 
 mod unity_interfaces;
 
-use unity_interfaces::IUnityInterfaces;
+use unity_interfaces::{
+    IUnityInterfaces,
+    UnityGfxDeviceEventType
+};
 
 struct PluginState {
-    logfile: PathBuf
+    logfile: PathBuf,
+    unity_interfaces: *const IUnityInterfaces
 }
 
 impl PluginState {
-    pub fn new() -> Self {
+    pub fn new(unity_interfaces: *const IUnityInterfaces) -> Self {
         let mut logfile = env::current_dir().unwrap();
         logfile.push("pathfinder-plugin.log");
         let mut plugin = PluginState {
             logfile,
+            unity_interfaces
         };
-        plugin.log("Pathfinder plugin initialized.");
+        plugin.initialize();
         plugin
+    }
+
+    fn initialize(&mut self) {
+        self.log("Pathfinder plugin initialized.");
+        unsafe {
+            let gfx = (*self.unity_interfaces).get_unity_graphics();
+            assert!(!gfx.is_null());
+            let renderer = ((*gfx).get_renderer)();
+            self.log(format!("Renderer is {:?}.", renderer));
+            ((*gfx).register_device_event_callback)(handle_unity_device_event);
+        }
     }
 
     pub fn log<T: AsRef<str>>(&mut self, msg: T) {
@@ -36,19 +52,23 @@ impl PluginState {
 impl Drop for PluginState {
     fn drop(&mut self) {
         self.log("Shutting down Pathfinder plugin.");
+        // TODO: We should ideally unregister our device event callback here, but
+        // we never seem to actually get called, so I guess it's not that urgent...
     }
 }
 
 static mut PLUGIN_STATE: Option<PluginState> = None;
 
+extern "stdcall" fn handle_unity_device_event(event_type: UnityGfxDeviceEventType) {
+    get_plugin_state_mut().log(format!("Unity graphics event occurred: {:?}", event_type));
+}
+
 // This is called by Unity when the plugin is loaded.
 #[no_mangle]
-pub extern "stdcall" fn UnityPluginLoad(ptr: *mut IUnityInterfaces) {
+pub extern "stdcall" fn UnityPluginLoad(unity_interfaces: *const IUnityInterfaces) {
     unsafe {
         assert!(PLUGIN_STATE.is_none());
-        let mut plugin = PluginState::new();
-        let gfx = (*ptr).get_unity_graphics();
-        plugin.log(format!("Unity graphics is {:?}.", gfx));
+        let plugin = PluginState::new(unity_interfaces);
         PLUGIN_STATE = Some(plugin);
     }
 }
--- src/unity_interfaces.rs
+++ src/unity_interfaces.rs
@@ -5,12 +5,8 @@
 
 use libc::{
     c_ulonglong,
+    c_int,
     c_void
-};
-
-pub const I_UNITY_GRAPHICS_GUID: UnityInterfaceGUID = UnityInterfaceGUID {
-    high: 0x7CBA0A9CA4DDB544,
-    low: 0x8C5AD4926EB17B11
 };
 
 #[repr(C)]
@@ -19,20 +15,67 @@ pub struct UnityInterfaceGUID {
     pub low: c_ulonglong,
 }
 
+// Type alias to indicate that we don't need a struct member, so we won't bother writing
+// a (potentially incorrect) typing for it. However, it does occupy the size of a C void
+// pointer in a struct, so we want to declare it for memory layout purposes.
+type Unneeded = *const c_void;
+
 #[repr(C)]
 pub struct IUnityInterfaces {
-    // I have no idea how to put a struct on the stack in an ABI, so I'm not going to use
+    // I have no idea how passing a struct by value works in FFI, so I'm not going to use
     // the non-split versions of the API.
-    get_interface: *const c_void,
-    register_interface: *const c_void,
+    get_interface: Unneeded,
+    register_interface: Unneeded,
 
     get_interface_split: extern "stdcall" fn(guid_high: c_ulonglong, guid_low: c_ulonglong) -> *const c_void,
-    // TODO: Fill in the call signature for this function.
-    register_interface_split: *const c_void,
+    register_interface_split: Unneeded,
 }
 
 impl IUnityInterfaces {
-    pub fn get_unity_graphics(&self) -> *const c_void {
-        (self.get_interface_split)(I_UNITY_GRAPHICS_GUID.high, I_UNITY_GRAPHICS_GUID.low)
+    pub fn get_unity_graphics(&self) -> *const IUnityGraphics {
+        (self.get_interface_split)(I_UNITY_GRAPHICS_GUID.high, I_UNITY_GRAPHICS_GUID.low) as *const IUnityGraphics
     }
 }
+
+#[derive(Debug)]
+pub enum UnityGfxRenderer {
+    // OpenGL            =  0, // Legacy OpenGL, removed
+    // D3D9              =  1, // Direct3D 9, removed
+    D3D11             =  2, // Direct3D 11
+    GCM               =  3, // PlayStation 3
+    Null              =  4, // "null" device (used in batch mode)
+    OpenGLES20        =  8, // OpenGL ES 2.0
+    OpenGLES30        = 11, // OpenGL ES 3.0
+    GXM               = 12, // PlayStation Vita
+    PS4               = 13, // PlayStation 4
+    XboxOne           = 14, // Xbox One
+    Metal             = 16, // iOS Metal
+    OpenGLCore        = 17, // OpenGL core
+    D3D12             = 18, // Direct3D 12
+    Vulkan            = 21, // Vulkan
+    Nvn               = 22, // Nintendo Switch NVN API
+    XboxOneD3D12      = 23  // MS XboxOne Direct3D 12
+}
+
+#[derive(Debug)]
+pub enum UnityGfxDeviceEventType
+{
+    Initialize     = 0,
+    Shutdown       = 1,
+    BeforeReset    = 2,
+    AfterReset     = 3,
+}
+
+type IUnityGraphicsDeviceEventCallback = extern "stdcall" fn(event_type: UnityGfxDeviceEventType);
+
+pub struct IUnityGraphics {
+    pub get_renderer: extern "stdcall" fn() -> UnityGfxRenderer,
+    pub register_device_event_callback: extern "stdcall" fn(cb: IUnityGraphicsDeviceEventCallback),
+    pub unregister_device_event_callback: extern "stdcall" fn(cb: IUnityGraphicsDeviceEventCallback),
+    pub reserve_event_id_range: extern "stdcall" fn(count: c_int) -> c_int
+}
+
+pub const I_UNITY_GRAPHICS_GUID: UnityInterfaceGUID = UnityInterfaceGUID {
+    high: 0x7CBA0A9CA4DDB544,
+    low: 0x8C5AD4926EB17B11
+};
