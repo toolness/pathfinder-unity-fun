58d50a9e4b768f371f180b3b3f026b4cbfed44b6
Atul Varma <varmaa@gmail.com>
2019-05-25 11:58:05 -0400

    Copy resources to StreamingAssets folder.

--- build_plugin.bat
+++ build_plugin.bat
@@ -2,4 +2,8 @@
 
 mkdir dist\unity-project_Data\Plugins 2>nul
 
-cargo build && copy target\debug\pathfinder_c_api_fun.dll unity-project\Assets\GfxPluginPathfinder.dll && copy target\debug\pathfinder_c_api_fun.dll dist\unity-project_Data\Plugins\GfxPluginPathfinder.dll
+cargo build && ^
+copy target\debug\pathfinder_c_api_fun.dll unity-project\Assets\GfxPluginPathfinder.dll && ^
+copy target\debug\pathfinder_c_api_fun.dll dist\unity-project_Data\Plugins\GfxPluginPathfinder.dll && ^
+xcopy /q /s /y pathfinder\resources unity-project\Assets\StreamingAssets\pathfinder\ && ^
+xcopy /q /s /y pathfinder\resources dist\unity-project_Data\StreamingAssets\pathfinder\
