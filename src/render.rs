243ed0fb6d2bff6cae08e3a199dca37e3d6735f4
Atul Varma <varmaa@gmail.com>
2019-05-27 07:39:25 -0400

    Recreate renderer on window resize.

--- src/render.rs
+++ src/render.rs
@@ -8,30 +8,65 @@ use pathfinder_renderer::gpu::renderer::DestFramebuffer;
 use pathfinder_renderer::concurrent::rayon::RayonExecutor;
 use pathfinder_renderer::concurrent::scene_proxy::SceneProxy;
 use pathfinder_renderer::options::RenderOptions;
+use pathfinder_renderer::gpu::renderer::Renderer as PathfinderRenderer;
 
+use crate::logging::log;
 use crate::gl_util::{get_viewport_size, get_draw_framebuffer_binding};
 
+fn get_current_window_size() -> Point2DI32 {
+    let (_, _, width, height) = get_viewport_size();
+    Point2DI32::new(width, height)
+}
+
+fn build_renderer(
+    window_size: Point2DI32,
+    loader: &FilesystemResourceLoader
+) -> PathfinderRenderer<GLDevice> {
+    let fbo_id = get_draw_framebuffer_binding();
+    pathfinder_renderer::gpu::renderer::Renderer::new(
+        GLDevice::new(GLVersion::GL3, fbo_id),
+        loader,
+        DestFramebuffer::full_window(window_size)
+    )
+}
+
 pub struct Renderer {
-    renderer: pathfinder_renderer::gpu::renderer::Renderer<GLDevice>,
+    loader: FilesystemResourceLoader,
+    renderer: PathfinderRenderer<GLDevice>,
     window_size: Point2DI32
 }
 
 impl Renderer {
     pub fn new(resources_dir: PathBuf) -> Self {
-        let (_, _, width, height) = get_viewport_size();
-        let window_size = Point2DI32::new(width, height);
-        let fbo_id = get_draw_framebuffer_binding();
+        let window_size = get_current_window_size();
         let loader = FilesystemResourceLoader { directory: resources_dir };
-        let renderer = pathfinder_renderer::gpu::renderer::Renderer::new(
-            GLDevice::new(GLVersion::GL3, fbo_id),
-            &loader,
-            DestFramebuffer::full_window(window_size)
-        );
+        let renderer = build_renderer(window_size, &loader);
 
-        Renderer { renderer, window_size }
+        Renderer { loader, renderer, window_size }
+    }
+
+    fn check_window_size(&mut self) {
+        let window_size = get_current_window_size();
+        if window_size != self.window_size {
+            let fb = get_draw_framebuffer_binding();
+            log(format!(
+                "Window size changed from {:?} to {:?} w/ fb {}.",
+                self.window_size,
+                window_size,
+                fb
+            ));
+            self.window_size = window_size;
+
+            // When Unity changes its window size, it often also changes the
+            // current draw framebuffer, and since Pathfinder doesn't currently
+            // seem to support retargeting that, we need to create a whole new
+            // renderer from scratch, which is a bummer.
+            self.renderer = build_renderer(self.window_size, &self.loader);
+        }
     }
 
     pub fn render(&mut self) {
+        self.check_window_size();
         let renderer = &mut self.renderer;
 
         // Make a canvas. We're going to draw a house.
