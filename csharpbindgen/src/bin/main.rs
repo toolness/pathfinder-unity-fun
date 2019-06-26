ce9fb6b31dd8667d5b176ed68295b92933cb96df
Atul Varma <varmaa@gmail.com>
2019-06-26 19:07:22 -0400

    Add csharpbindgen crate.

---
+++ csharpbindgen/src/bin/main.rs
@@ -0,0 +1,19 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let mut path = env::current_dir().unwrap();
+    path.push("..");
+    path.push("src");
+    path.push("pathfinder_unity_api.rs");
+
+    if !path.exists() {
+        panic!("Expected file to exist: {}", path.to_string_lossy());
+    }
+
+    let code = fs::read_to_string(path).expect("unable to read rust source file");
+
+    let syntax = syn::parse_file(&code).expect("unable to parse rust source file");
+
+    println!("{:#?}", syntax);
+}
