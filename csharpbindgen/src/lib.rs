261e554e88e68bb1daae9c2904eee272f6177caf
Atul Varma <varmaa@gmail.com>
2019-06-27 06:24:26 -0400

    Return string w/ code instead of printing to stdout.

--- csharpbindgen/src/lib.rs
+++ csharpbindgen/src/lib.rs
@@ -2,24 +2,23 @@ use syn::Item;
 
 pub fn create_csharp_bindings(rust_code: &String) -> String {
     let syntax = syn::parse_file(&rust_code).expect("unable to parse rust source file");
+    let mut lines = Vec::new();
 
     // println!("{:#?}", syntax);
 
     for item in syntax.items.iter() {
         match item {
             Item::Struct(item_struct) => {
-                println!("// TODO: Define struct {}", item_struct.ident.to_string());
+                lines.push(format!("// TODO: Define struct {}", item_struct.ident.to_string()));
             },
             Item::Fn(item_fn) => {
                 if item_fn.abi.is_some() {
-                    println!("// TODO: Define fn {}()", item_fn.ident.to_string());
+                    lines.push(format!("// TODO: Define fn {}()", item_fn.ident.to_string()));
                 }
             },
             _ => {}
         }
     }
 
-    let output = String::new();
-
-    output
+    lines.join("\n")
 }
