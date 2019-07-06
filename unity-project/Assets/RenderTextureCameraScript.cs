29a2997bf0de6e9bc743132ad84b245ec9357719
Atul Varma <varmaa@gmail.com>
2019-07-06 12:06:28 -0400

    Animate size of text on cube.

--- unity-project/Assets/RenderTextureCameraScript.cs
+++ unity-project/Assets/RenderTextureCameraScript.cs
@@ -3,20 +3,38 @@
 public class RenderTextureCameraScript : MonoBehaviour
 {
     private PFCanvasFontContext fontContext;
+    public float fontSize;
+    private float fontVelocity;
+    private float maxFontSize;
+    private float minFontSize;
 
     // Start is called before the first frame update
     void Start()
     {
         fontContext = new PFCanvasFontContext();
+        fontSize = 10.0f;
+        fontVelocity = 1.0f;
+        maxFontSize = 160.0f;
+        minFontSize = 10.0f;
     }
 
     public void OnPostRender() {
         var canvas = new PFCanvas(fontContext, new Vector2(256, 256));
 
-        canvas.SetStrokeStyle(Color.red);
-        canvas.SetFontSize(128.0f);
-        canvas.FillText("Yo!", new Vector2(10.0f, 128.0f));
+        canvas.SetFontSize(fontSize);
+        canvas.FillText("Yo!", new Vector2(10.0f, fontSize));
 
         canvas.QueueForRendering(2);
+    }
+
+    public void Update() {
+        fontSize += fontVelocity;
+        if (fontSize > maxFontSize) {
+            fontSize = maxFontSize;
+            fontVelocity = -Mathf.Abs(fontVelocity);
+        } else if (fontSize < minFontSize) {
+            fontSize = minFontSize;
+            fontVelocity = Mathf.Abs(fontVelocity);
+        }
     }
 }
